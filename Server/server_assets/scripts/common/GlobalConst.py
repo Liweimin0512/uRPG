f7bb773b6f1137384326ad48dd00ae1f20dcd5dd
jeremyLee0512 <2410250284@qq.com>
2019-10-22 08:05:34 +0800

    临时全局常量

---
+++ Server/server_assets/scripts/common/GlobalConst.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+"""
+GC_OK								= 0x000
+
+# 技能相关
+GC_SKILL_MP_NOT_ENGOUH				= 0x001		# 法力值不足
+GC_SKILL_ENTITY_DEAD				= 0x002		# Entity已经死亡
+GC_SKILL_ENTITY_REST				= 0x003		# 被麻痹
+
+# 不同demo所对应的地图
+g_demoMaps = {
+	b'' : 1,
+	b'' : 2,
+	b'' : 3,
+	b'' : 4
+}
