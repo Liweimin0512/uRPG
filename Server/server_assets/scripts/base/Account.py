0b1608329b5aec174304a446c23ac7cfd03c4932
jeremyLee0512 <2410250284@qq.com>
2019-10-23 08:15:31 +0800

    server account

--- Server/server_assets/scripts/base/Account.py
+++ Server/server_assets/scripts/base/Account.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import time
 
+import GlobalConst
 import KBEngine
+import d_spaces
 from AVATAR_INFO import TAvatarInfo
 from KBEDebug import *
+
 
 class Account(KBEngine.Proxy):
 	"""
@@ -33,6 +36,23 @@ class Account(KBEngine.Proxy):
 			self.client.onCreateAvatarResult(3, avatarinfo)
 			return
 
+		spaceUType = GlobalConst.g_demoMaps.get(self.getClientDatas()[0], 1)
+		spaceData = d_spaces.datas.get(spaceUType)
+
+		props = {
+			"name" 			: name,
+			"roleType"		: roleType,
+			"level"			: 1,
+			"position"		: spaceData.get("spawnPos", (0, -5, -10)),
+		}
+
+		avatar = KBEngine.createEntityLocally('Avatar', props)
+		if avatar:
+			avatar.writeToDB(self._onAvatarSaved)
+
+		DEBUG_MSG("Account[%i].reqCreateAvatar:%s. spaceUType=%i, spawnPos=%s.\n"
+			%(self.id, name,avatar.cellData["spaceUType"], spaceData.get("spawnPos",(0,0,0))))
+
 	def reqRemoveAvatar(self, name):
 		"""
 		请求删除一个角色
@@ -49,14 +69,6 @@ class Account(KBEngine.Proxy):
 
 		self.client.onRemoveAvatar(found)
 
-	def reqSelectAvatar(self, dbid):
-		"""
-		选择某个角色时候，获取详细信息。并设为选中角色
-		:param dbid:
-		:return:
-		"""
-		pass
-
 	def reqEnterGame(self, dbid):
 		"""
 		选择摸个角色，请求进入游戏
@@ -71,7 +83,7 @@ class Account(KBEngine.Proxy):
 				# 当角色创建好之后，account会调用giveClientTo将客户端控制权（可理解为网络连接与某个实体的绑定）切换到Avatar身上，
 				# 之后客户端各种输入输出都通过服务器上这个Avatar来代理，任何proxy实体获得控制权都会调用onEntitiesEnabled
 				# Avatar继承了Teleport，Teleport.onEntitiesEnabled会将玩家创建在具体的场景中
-				KBEngine.createEntityFromDBID("Avatar", dbid, self.__onAvatarCreated)
+				KBEngine.createEntityFromDBID("Avatar", dbid, self._onAvatarEnter)
 			else:
 				ERROR_MSG("Account[%i]::selectAvatarGame: not found dbid(%i)" % (self.id, dbid))
 		else:
@@ -81,36 +93,44 @@ class Account(KBEngine.Proxy):
 	#         Callbacks
 	#------------------------------
 
-	def onTimer(self, id, userArg):
-		"""
-		KBEngine method.
-		使用addTimer后， 当时间到达则该接口被调用
-		@param id		: addTimer 的返回值ID
-		@param userArg	: addTimer 最后一个参数所给入的数据
-		"""
-		DEBUG_MSG(id, userArg)
-		
 	def onClientEnabled(self):
 		"""
 		KBEngine method.
 		该entity被正式激活为可使用， 此时entity已经建立了client对应实体， 可以在此创建它的
 		cell部分。
 		"""
-		INFO_MSG("account[%i] entities enable. entityCall:%s" % (self.id, self.client))
+		INFO_MSG("account[%i] entities enable. entityCall:%s" %
+				 (self.id, self.client))
 
 	def onLogOnAttempt(self, ip, port, password):
 		"""
 		KBEngine method.
 		客户端登陆失败时会回调到这里
 		"""
-		INFO_MSG(ip, port, password)
+		# 如果一个在线的账号被一个客户端登陆并且onLogOnAttempt返回允许
+		# 那么会踢掉之前的客户端连接
+		# 那么此时self.activeAvatar可能不为None， 常规的流程是销毁这个角色等新客户端上来重新选择角色进入
+		if self.activeAvatar:
+			if self.activeAvatar.client is not None:
+				self.activeAvatar.giveClientTo(self)
+
+			self.relogin = time.time()
+			# self.activeAvatar.destroySelf()
+			if self.activeAvatar.cell is not None:
+				self.activeAvatar.destroyCellEntity()
+			self.activeAvatar = None
+
 		return KBEngine.LOG_ON_ACCEPT
-		
+
 	def onClientDeath(self):
 		"""
 		KBEngine method.
 		客户端对应实体已经销毁
 		"""
+		if self.activeAvatar:
+			self.activeAvatar.accountEntity = None
+			self.activeAvatar = None
+
 		DEBUG_MSG("Account[%i].onClientDeath:" % self.id)
 		self.destroy()
 
@@ -118,17 +138,51 @@ class Account(KBEngine.Proxy):
 		"""
 		entity销毁时回调
 		"""
-		pass
+		DEBUG_MSG("Account::onDestroy: %i." % self.id)
 
-	def _onAvatarCreated(self, baseRef, dbid, wasActive):
+		if self.activeAvatar:
+			self.activeAvatar.accountEntity = None
+
+			try:
+				self.activeAvatar.destroySelf()
+			except:
+				pass
+
+			self.activeAvatar = None
+
+	def _onAvatarEnter(self, baseRef, dbid, wasActive):
 		"""
 		选择角色进入游戏时被回调
 		:param baseRef:
 		:param dbid:
 		:param wasActive:
-		:return:
 		"""
-		pass
+		if wasActive:
+			ERROR_MSG("Account::__onAvatarCreated:(%i): this character is in world now!" % (self.id))
+			baseRef.accountEntity = self
+			self.activeAvatar = baseRef
+			self.giveClientTo(baseRef)
+			return
+		if baseRef is None:
+			ERROR_MSG("Account::__onAvatarCreated:(%i): the character you wanted to created is not exist!" % (self.id))
+			return
+
+		avatar = KBEngine.entities().get(baseRef.id)
+		if avatar is None:
+			ERROR_MSG("Account::__onAvatarCreated:(%i): when character was created, it died as well!" % (self.id))
+			return
+
+		if self.isDestroyed:
+			ERROR_MSG("Account::__onAvatarCreated:(%i): i dead, will the destroy of Avatar!" % (self.id))
+			avatar.destroy()
+			return
+
+		info = self.characters[dbid]
+		profesional = info[2]
+
+		avatar.accountEntity = self
+		self.activeAvatar = avatar
+		self.giveClientTo(avatar)
 
 	def _onAvatarSaved(self, success, avatar):
 		"""
