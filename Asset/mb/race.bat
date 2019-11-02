e7b2f052f231000ceb1068478b5b58ec95f363ee
jeremyLee0512 <2410250284@qq.com>
2019-11-02 20:33:35 +0800

    创建角色相关

--- Asset/mb/race.bat
+++ Asset/mb/race.bat
@@ -1,7 +1,7 @@
 call basePath.bat
 @echo off
-set pydatas=%ktpydatas%/d_entities.py
-set excel1=%ktexcels%/xlsxs/entities.xlsx
+set pydatas=%ktpydatas%/d_racetab.py
+set excel1=%ktexcels%/xlsxs/race.xlsx
 echo on
 python ../../Tools/xlsx2py/xlsx2py/xlsx2py.py %pydatas% %excel1%
 if not defined ktall (ping -n 30 127.1>nul)
