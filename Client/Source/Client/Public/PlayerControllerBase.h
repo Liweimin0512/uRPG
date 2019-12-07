76d8cf6ee8db4293ae7d590dab7f35297834815c
jeremyLee0512 <2410250284@qq.com>
2019-12-07 08:53:28 +0800

    动画蓝图

---
+++ Client/Source/Client/Public/PlayerControllerBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PlayerControllerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLIENT_API APlayerControllerBase : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
